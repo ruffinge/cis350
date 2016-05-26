ff16cb6b9e4b0d24e00b06491d341526df090376
Rekodr <rekodr@mail.gvsu.edu>
2016-05-25 20:56:51 -0400

    add icon to the app

--- moviefinder/src/main/java/moviefinder/MovieFinder.java
+++ moviefinder/src/main/java/moviefinder/MovieFinder.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MovieFinder extends Application{
@@ -14,6 +15,7 @@ public class MovieFinder extends Application{
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("moviefinder/MainView.fxml"));
         primaryStage.setTitle("Movie DB");
+        primaryStage.getIcons().add(new Image("bird.png"));
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
