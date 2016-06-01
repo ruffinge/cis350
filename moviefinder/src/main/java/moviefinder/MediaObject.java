ea0f45b8f07c7cdb58384028516983e2c9bbf355
Ethan Ruffing <ruffinge@gmail.com>
2016-05-31 20:20:46 -0400

    Documentation and cleanup for MediaObject

--- moviefinder/src/main/java/moviefinder/MediaObject.java
+++ moviefinder/src/main/java/moviefinder/MediaObject.java
@@ -1,7 +1,5 @@
 package moviefinder;
 
-import java.io.IOException;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
@@ -9,25 +7,48 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
+/**
+ * A container for a display of information about an item in TMDB.
+ */
 public class MediaObject {
 
+    /**
+     * An image to be displayed.
+     */
     private ImageView image;
 
+    /**
+     * The title of the media.
+     */
     @FXML
     private Text title;
 
+    /**
+     * A VBox for the media information.
+     */
     private VBox mediaVBox;
 
-    MediaObject(Image pImage, String pTitle, MainViewController mainController) {
+    /**
+     * Construct a new MediaObject.
+     *
+     * @param pImage
+     *            The image for the media item.
+     * @param pTitle
+     *            The title for the media item.
+     * @param mainController
+     *            The controller for the main window.
+     */
+    public MediaObject(final Image pImage, final String pTitle,
+            final MainViewController mainController) {
         try {
-        	FXMLLoader mediaLoader = new FXMLLoader(getClass().getClassLoader()
-        			.getResource("view/MediaObject.fxml"));
-        	mediaVBox = mediaLoader.load();
+            FXMLLoader mediaLoader = new FXMLLoader(getClass().getClassLoader()
+                    .getResource("view/MediaObject.fxml"));
+            mediaVBox = mediaLoader.load();
             image = (ImageView) mediaVBox.getChildren().get(0);
             title = (Text) mediaVBox.getChildren().get(1);
             MediaController controller = mediaLoader.getController();
             controller.initialize(mainController);
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -38,8 +59,12 @@ public class MediaObject {
         title.setText(pTitle);
     }
 
-    public VBox getVBox() {
+    /**
+     * Accessor for the mediaVBox.
+     *
+     * @return The mediaVBox.
+     */
+    public final VBox getVBox() {
         return mediaVBox;
     }
-
 }
